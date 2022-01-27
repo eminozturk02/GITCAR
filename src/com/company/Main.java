4fc78f78ff053b04d0b3c2b5e31151c66a219051
mehmetozturkeri <eminozturk02@gmail.com>
2022-01-27 11:56:24 -0600

    here are the changes that you asked me to make please make up your mind

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
 	// the basic structure
         CarBasics car=new CarBasics();
         car.color="green";
-        car.number="789";
-        car.engine="544444cc";
-        car.door="3";
+        car.number="88877375";
+        car.engine="53456744444cc";
+        car.door="4";
         System.out.println(car.color);
 
     }
